7350bf3d76137928229ccba75b0d8269de0fc31c
seregapashchenko97-cmd <seregapashchenko97@gmail.com>
2026-05-29 21:18:12 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,1 @@
-from PIL import Image
-import requests
-
-def test():
-response = requests.get("https://picsum.photos/500")
-img = Image.open(requests.get("https://picsum.photos/500", stream=True).raw)
-img.save("test.png")
-
-print("WORKING")
+print("HELLO")
