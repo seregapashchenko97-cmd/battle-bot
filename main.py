b6a72cf63b9a1a84aaab23907ff72a14c2e75a92
seregapashchenko97-cmd <seregapashchenko97@gmail.com>
2026-05-30 05:56:27 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,16 @@
 import subprocess
 
 cmd = [
-"ffmpeg",
-"-f",
-"lavfi",
-"-i",
-"color=c=red:s=1080x1920:d=3",
-"-c:v",
-"libx264",
-"-pix_fmt",
-"yuv420p",
-"test.mp4",
-"-y"
+    "ffmpeg",
+    "-f", "lavfi",
+    "-i", "color=c=red:s=540x960:d=3",
+    "-c:v", "libx264",
+    "-pix_fmt", "yuv420p",
+    "test.mp4",
+    "-y"
 ]
 
-result = subprocess.run(
-cmd,
-capture_output=True,
-text=True
-)
+result = subprocess.run(cmd, capture_output=True, text=True)
 
 print("RETURN CODE:", result.returncode)
 print(result.stderr)
