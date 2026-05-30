3ed64531e784f979831bfad1aa06425e4dcfd84b
seregapashchenko97-cmd <seregapashchenko97@gmail.com>
2026-05-30 06:42:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,5 @@
-import subprocess
+from PIL import ImageFont
 
-cmd = [
-    "ffmpeg",
-    "-f", "lavfi",
-    "-i", "color=c=red:s=540x960:d=3",
-    "-c:v", "libx264",
-    "-pix_fmt", "yuv420p",
-    "test.mp4",
-    "-y"
-]
+font = ImageFont.truetype("Anton-Regular.ttf", 120)
 
-result = subprocess.run(cmd, capture_output=True, text=True)
-
-print("RETURN CODE:", result.returncode)
-print(result.stderr)
+print("ANTON OK")
