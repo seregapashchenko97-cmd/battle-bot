a2239ac8e8e1105f396fcf61655a54bb5c1dd363
seregapashchenko97-cmd <seregapashchenko97@gmail.com>
2026-05-30 13:09:01 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,51 @@
-import os
 import asyncio
 
-from aiogram import Bot, Dispatcher
+from aiogram import Bot, Dispatcher, F
 from aiogram.filters import CommandStart
-from aiogram.types import Message
+from aiogram.types import (
+    Message,
+    ReplyKeyboardMarkup,
+    KeyboardButton
+)
 
-BOT_TOKEN = "8330007893:AAGBWfwgoF3dxVJvBQTEADQnK-kCQRz40BE"
+BOT_TOKEN = "ТВОЙ_ТОКЕН"
+
 bot = Bot(BOT_TOKEN)
 dp = Dispatcher()
+
+keyboard = ReplyKeyboardMarkup(
+    keyboard=[
+        [KeyboardButton(text="🎲 Генерация вариантов")]
+    ],
+    resize_keyboard=True
+)
 
 
 @dp.message(CommandStart())
 async def start(message: Message):
     await message.answer(
-        "🎲 Генератор VS запущен"
+        "🎲 Генератор VS запущен",
+        reply_markup=keyboard
     )
+
+
+@dp.message(F.text == "🎲 Генерация вариантов")
+async def generate(message: Message):
+
+    variants = [
+        "❤️ Любовь VS 💰 Деньги",
+        "🍔 Бургер VS 🎫 Билет на концерт",
+        "🏝 Отдых VS 🚗 Новая машина",
+        "🐶 Собака VS 🐱 Кот",
+        "✈️ Путешествие VS 🏠 Дом мечты"
+    ]
+
+    text = "🔥 Варианты:\n\n"
+
+    for i, item in enumerate(variants, start=1):
+        text += f"{i}. {item}\n"
+
+    await message.answer(text)
 
 
 async def main():
